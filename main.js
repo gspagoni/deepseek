e3ff135037a40db314a1b9554545f5d2a5fc6f8d
Giampaolo Spagoni <giampaolo.spagoni@infor.com>
2025-02-19 19:35:29 +0100

    changed display messages

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const { app, BrowserWindow, ipcMain } = require("electron");
+const { app, BrowserWindow, ipcMain, dialog } = require("electron");
 const { autoUpdater } = require("electron-updater");
 const log = require("electron-log");
 
-// Configure logging
+// Configura il logger
 autoUpdater.logger = log;
 autoUpdater.logger.transports.file.level = "info";
 
@@ -20,34 +20,68 @@ function createWindow() {
 
   mainWindow.loadFile("index.html");
 
-  // Check for updates
-  autoUpdater.checkForUpdatesAndNotify();
+  // Controlla gli aggiornamenti
+  autoUpdater.checkForUpdates();
 }
 
-app.whenReady().then(() => {
-  createWindow();
-  log.info("App version:", app.getVersion());
+app.whenReady().then(createWindow);
+
+// Eventi dell'auto-updater
+autoUpdater.on("update-available", async (info) => {
+  log.info("Update available:", info);
+
+  // Mostra una finestra di dialogo per chiedere all'utente se vuole scaricare l'aggiornamento
+  const { response } = await dialog.showMessageBox(mainWindow, {
+    type: "info",
+    title: "Aggiornamento disponibile",
+    message: "È disponibile una nuova versione. Vuoi scaricarla ora?",
+    buttons: ["Sì", "No"],
+    defaultId: 0,
+    cancelId: 1,
+  });
+
+  if (response === 0) {
+    // L'utente ha scelto di scaricare l'aggiornamento
+    autoUpdater.downloadUpdate();
+  } else {
+    // L'utente ha scelto di non scaricare l'aggiornamento
+    log.info("Download annullato dall'utente");
+  }
 });
 
-// Auto-updater events
-autoUpdater.on("update-available", () => {
-  log.info("Update available");
-  mainWindow.webContents.send("update_available");
-});
+autoUpdater.on("update-downloaded", async (info) => {
+  log.info("Update downloaded:", info);
 
-autoUpdater.on("update-downloaded", () => {
-  log.info("Update downloaded");
-  mainWindow.webContents.send("update_downloaded");
+  // Mostra una finestra di dialogo per chiedere all'utente se vuole riavviare l'app
+  const { response } = await dialog.showMessageBox(mainWindow, {
+    type: "info",
+    title: "Aggiornamento scaricato",
+    message: "L'aggiornamento è stato scaricato. Vuoi riavviare l'app ora?",
+    buttons: ["Riavvia", "Più tardi"],
+    defaultId: 0,
+    cancelId: 1,
+  });
+
+  if (response === 0) {
+    // L'utente ha scelto di riavviare l'app
+    autoUpdater.quitAndInstall();
+  } else {
+    // L'utente ha scelto di riavviare più tardi
+    log.info("Riavvio posticipato dall'utente");
+  }
 });
 
 autoUpdater.on("error", (error) => {
-  log.error("Error in auto-updater:", error);
-  mainWindow.webContents.send("error");
-});
+  log.error("Errore durante il controllo degli aggiornamenti:", error);
 
-// Quit and install the update
-ipcMain.on("restart_app", () => {
-  autoUpdater.quitAndInstall();
+  // Mostra una finestra di dialogo per l'errore
+  dialog.showMessageBox(mainWindow, {
+    type: "error",
+    title: "Errore",
+    message:
+      "Si è verificato un errore durante il controllo degli aggiornamenti.",
+    buttons: ["OK"],
+  });
 });
 
 app.on("window-all-closed", () => {
