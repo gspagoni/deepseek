938e4dd010c35d8335659db25908d5d615ace111
Giampaolo Spagoni <giampaolo.spagoni@infor.com>
2025-02-19 20:01:39 +0100

    added dialog.html

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 const { app, BrowserWindow, ipcMain, dialog } = require("electron");
 const { autoUpdater } = require("electron-updater");
 const log = require("electron-log");
+const path = require("path");
 
 // Configura il logger
 autoUpdater.logger = log;
 autoUpdater.logger.transports.file.level = "info";
 
 // Disabilita le notifiche predefinite
-autoUpdater.autoDownload = false; // Disabilita il download automatico
-autoUpdater.autoInstallOnAppQuit = false; // Disabilita l'installazione automatica all'uscita
+autoUpdater.autoDownload = false;
+autoUpdater.autoInstallOnAppQuit = false;
 
 let mainWindow;
+let dialogWindow;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -28,51 +30,90 @@ function createWindow() {
   autoUpdater.checkForUpdates();
 }
 
+function createDialog(options) {
+  dialogWindow = new BrowserWindow({
+    width: 400,
+    height: 300,
+    resizable: false,
+    frame: false, // Rimuove la barra del titolo
+    show: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  dialogWindow.loadFile(options.file);
+
+  // Passa i dati alla finestra di dialogo
+  dialogWindow.webContents.on("did-finish-load", () => {
+    dialogWindow.webContents.send("dialog-options", options);
+  });
+
+  dialogWindow.once("ready-to-show", () => {
+    dialogWindow.show();
+  });
+
+  return dialogWindow;
+}
+
 app.whenReady().then(createWindow);
 
 // Eventi dell'auto-updater
 autoUpdater.on("update-available", async (info) => {
   log.info("Update available:", info);
 
-  // Mostra una finestra di dialogo per chiedere all'utente se vuole scaricare l'aggiornamento
-  const { response } = await dialog.showMessageBox(mainWindow, {
-    type: "info",
+  // Crea una finestra di dialogo personalizzata
+  const dialogOptions = {
+    file: "dialog.html",
     title: "Aggiornamento disponibile",
     message: "È disponibile una nuova versione. Vuoi scaricarla ora?",
     buttons: ["Sì", "No"],
-    defaultId: 0,
-    cancelId: 1,
-  });
+  };
 
-  if (response === 0) {
-    // L'utente ha scelto di scaricare l'aggiornamento
-    autoUpdater.downloadUpdate();
-  } else {
-    // L'utente ha scelto di non scaricare l'aggiornamento
-    log.info("Download annullato dall'utente");
-  }
+  const dialogWindow = createDialog(dialogOptions);
+
+  // Gestisci la risposta dell'utente
+  ipcMain.on("dialog-response", (event, response) => {
+    if (response === 0) {
+      // L'utente ha scelto di scaricare l'aggiornamento
+      autoUpdater.downloadUpdate();
+    } else {
+      // L'utente ha scelto di non scaricare l'aggiornamento
+      log.info("Download annullato dall'utente");
+    }
+
+    // Chiudi la finestra di dialogo
+    dialogWindow.close();
+  });
 });
 
 autoUpdater.on("update-downloaded", async (info) => {
   log.info("Update downloaded:", info);
 
-  // Mostra una finestra di dialogo per chiedere all'utente se vuole riavviare l'app
-  const { response } = await dialog.showMessageBox(mainWindow, {
-    type: "info",
+  // Crea una finestra di dialogo personalizzata
+  const dialogOptions = {
+    file: "dialog.html",
     title: "Aggiornamento scaricato",
     message: "L'aggiornamento è stato scaricato. Vuoi riavviare l'app ora?",
     buttons: ["Riavvia", "Più tardi"],
-    defaultId: 0,
-    cancelId: 1,
-  });
+  };
 
-  if (response === 0) {
-    // L'utente ha scelto di riavviare l'app
-    autoUpdater.quitAndInstall();
-  } else {
-    // L'utente ha scelto di riavviare più tardi
-    log.info("Riavvio posticipato dall'utente");
-  }
+  const dialogWindow = createDialog(dialogOptions);
+
+  // Gestisci la risposta dell'utente
+  ipcMain.on("dialog-response", (event, response) => {
+    if (response === 0) {
+      // L'utente ha scelto di riavviare l'app
+      autoUpdater.quitAndInstall();
+    } else {
+      // L'utente ha scelto di riavviare più tardi
+      log.info("Riavvio posticipato dall'utente");
+    }
+
+    // Chiudi la finestra di dialogo
+    dialogWindow.close();
+  });
 });
 
 autoUpdater.on("error", (error) => {
